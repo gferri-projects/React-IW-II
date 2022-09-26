b35147ad111d42383ede046e1a4c4ebefb2f687a
Julia Granado <79951728+gferri-projects@users.noreply.github.com>
2022-09-26 08:58:27 -0300

    Add files via upload

---
+++ aula_04/src/components/Form.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+function Form(){
+    function cadastrarUsuario(e){
+        e.preventDefault()
+        console.log(nome)
+        console.log('Cadastrou usuário!')
+    }
+    const [nome, setNome] = useState()
+    const [senha, setSenha] = useState()
+return(
+    <div>
+        <h1>Meu Cadastro</h1>
+        <form onSubmit={cadastrarUsuario}>
+            <div>
+                <label htmlFor="nome">Nome:</label>
+                <input 
+                    type="text"
+                    id="nome"
+                    name="nome"
+                    placeholder="Digite seu nome"
+                    onChange={(e) => setNome(e.target.value)}
+                 />
+            </div>
+            <div>
+                <label htmlFor="senha">Senha:</label>
+                <input 
+                    type="password"
+                    id="senha"
+                    name="senha"
+                    placeholder="Digite sua senha"
+                    onChange={(e) => setSenha(e.target.value)}
+                 />
+            </div>
+                <div>
+                    <input type="submit" value="ENVIAR"></input>
+                </div>
+
+        </form>
+    </div>
+)
+
+}
+
+export default Form
